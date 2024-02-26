cd5e276d4777121c704a1256091e9d20a549753f
Nikolay <6ajlamyt2013@users.noreply.github.com>
2024-02-26 15:48:31 +0300

    Added video integrity check

--- youtube.py
+++ youtube.py
@@ -1,9 +1,10 @@
 import youtube_dl
+import subprocess
 import os
 from pytube import YouTube
 
 
-channel_name = '@RyanCelsiusMusic' # input("Введите название YouTube канала: ")
+channel_name = input("Введите название YouTube канала: ")
 video_count = input("Сколько видео скачать ?: ")
 channel_url = "https://www.youtube.com/" + channel_name + "/videos"
 local_path = '/Users/nikolajtukmaceva/Desktop/'+ channel_name
@@ -33,11 +34,11 @@ def get_channel_videos(channel_url):
     return video_dict
 
 def download_video(vide_date):
-    counter = 0
+    index = 1
     for title, url in video_dict.items():
         video_url = 'https://www.youtube.com/watch?v=' + url
         title = title + '.mp4'
-        file_path = local_path + '/' + title
+        index += 1
 
         if not os.path.exists(local_path):
             os.makedirs(local_path)
@@ -45,18 +46,34 @@ def download_video(vide_date):
         current_video_name = os.listdir(local_path)
 
         if title in current_video_name:
-            print(" Видео уже загружено: ", title)
+            print(str(index) + " Видео уже загружено: ", title)
+            # video_path = local_path + '/' + title
+            # check_video_integrity(video_path)
         else:
             try:
                 yt = YouTube(video_url, use_oauth=True, allow_oauth_cache=True)
                 video = yt.streams.filter(file_extension='mp4').get_highest_resolution()
                 title = yt.title + '.mp4'
-                print("Загрузка видео: " + title)
+                print(str(index) + " Загрузка видео: " + title)
                 video.download(local_path, filename=title)
             except Exception as e:
                 print("Error:", e)
                 continue
-    print("Загрузка завершена: ")
+    print("Загрузка завершена")
+
+def check_video_integrity(video_path):
+    try:
+        result = subprocess.run(['ffmpeg', '-v', 'error', '-i', video_path, '-f', 'null', '-'],
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+        if result.returncode == 0:
+            print("Видео целостно: " + video_path)
+        else:
+            print("Видео повреждено или имеет ошибки: " + video_path)
+    except FileNotFoundError:
+        print("ffmpeg не найден. Убедитесь, что он установлен и добавлен в PATH.")
+
+
 
 
 video_dict = get_channel_videos(channel_url)
